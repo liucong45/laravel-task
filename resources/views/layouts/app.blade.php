88d2e55034136d35cc03bb21eb3bd3b0daed41ad
limbo <21531251@qq.com>
2018-12-03 12:28:16 +0800

    csrf-from

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,8 +18,8 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/font-awesome.min.css') }}" rel="stylesheet">
-    {{-- <link href="https://cdn.bootcss.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"> --}}
+    <!-- <link href="{{ asset('css/font-awesome.min.css') }}" rel="stylesheet"> -->
+    <link href="https://cdn.bootcss.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 </head>
 <body>
     <div id="app">
--- resources/views/projects/_createModal.blade.php
+++ resources/views/projects/_createModal.blade.php
@@ -8,18 +8,41 @@
         <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-            <h5 class="modal-title" id="createModalLabel">Modal title</h5>
+            <h5 class="modal-title" id="createModalLabel">新建项目</h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
             </button>
             </div>
+            {!! Form::open(['route'=>'projects.store']) !!}
             <div class="modal-body">
-            ...
+            <!-- <form action='/' method='POST' accept-charset="UTF-8">
+            <div class="form-group">
+                <label for="name">项目名称:</label>
+                <input class="form-control" name="name" type="text" value="" id="name">
+            </div>
+            <div class="form-group">
+                <label for="thumbnail">项目缩略图:</label>
+                <input class="form-control-file" name="thumbnail" type="file" id="thumbnail">
+            </div>
+                <input name="_token" type='hidden' value=' {{ csrf_token() }}'/>
+            </form> -->
+            
+                <div class="form-group">
+                {!! Form::label('name','项目名称:') !!}
+                {!! Form::text('name','',['class'=>'form-control']) !!}
+                </div>
+
+                <div class="form-group">
+                {!! Form::label('thumbnail','项目缩略图:') !!}
+                {!! Form::file('thumbnail',['class'=>'form-control-file']) !!}
+                </div>
             </div>
             <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Save changes</button>
+            <!-- <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button> -->
+            {!! Form::submit('新建项目',['class'=>'btn btn-primary']) !!}
             </div>
+            {!! Form::close() !!}
         </div>
         </div>
     </div>
